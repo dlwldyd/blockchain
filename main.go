9e92351f35aa0328b786f2d3a281b206be06fc11
이지용 <dlwldyd9303@naver.com>
2023-02-20 21:43:36 +0900

    서버 띄우기

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
-import "github.com/dlwldyd/coin/blockchain"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/dlwldyd/coin/blockchain"
+)
+
+const port string = ":3000";
 
 func main() {
 	blockchain := blockchain.GetInstance();
@@ -8,4 +16,12 @@ func main() {
 	blockchain.AddBlock("Third Block");
 
 	blockchain.ShowAllBlocks();
+
+	// localhost:3000/ 으로 들어오는 요청에 대한 핸들러
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "hello world"); // fmt.Print는 console에 출력하지만 fmt.Fprint는 파라미터로 들어가는 writer에 출력한다.
+	})
+
+	// 스프링에서 메인메서드라 보면 된다.
+	log.Fatal(http.ListenAndServe(port, nil));
 }
