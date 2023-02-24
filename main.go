09e346be7e77006945c16d64f15d2f8caf7b0cc2
이지용 <dlwldyd9303@naver.com>
2023-02-25 02:04:31 +0900

    blockchain add

--- main.go
+++ main.go
@@ -19,12 +19,24 @@ type homeData struct {
 	Blocks []*blockchain.Block
 }
 
+func add(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET" : // "/add" 로 get 요청을 받았을 때
+		templates.ExecuteTemplate(w, "add", nil) //add.gohtml을 렌더링
+	case "POST" : // "/add" 로 post 요청을 받았을 때
+		r.ParseForm() // form 데이터를 가져오기 전에 실행해야한다.
+		data := r.Form.Get("blockData") //form 데이터 중 blockData를 가져온다.
+		blockchain.GetInstance().AddBlock(data)
+		http.Redirect(w, r, "/", http.StatusPermanentRedirect) // "/"로 리다이렉트
+	}
+}
+
 func main() {
 	blockchain := blockchain.GetInstance();
-	blockchain.AddBlock("Second Block");
-	blockchain.AddBlock("Third Block");
+	// blockchain.AddBlock("Second Block");
+	// blockchain.AddBlock("Third Block");
 
-	blockchain.ShowAllBlocks();
+	// blockchain.ShowAllBlocks();
 
 	// parseGlob는 여러개의 템플릿을 한번에 로드할 때 사용한다.
 	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
@@ -44,8 +56,9 @@ func main() {
 		
 		// tmpl.Execute(w, data);
 		data := homeData{"Home", blockchain.AllBlocks()};
-		templates.ExecuteTemplate(w, "home", data)
+		templates.ExecuteTemplate(w, "home", data) // home.gohtml을 렌더링
 	})
+	http.HandleFunc("/add", add)
 
 	// 스프링에서 메인메서드라 보면 된다.
 	log.Fatal(http.ListenAndServe(port, nil));
