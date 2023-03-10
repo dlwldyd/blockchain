1e5c0e736e6d7c1124e2490c82fd1562721881d4
lewis.zd <lewis.zd@LEWIS-ZD-NT-2023-02.local>
2023-03-10 15:21:27 +0900

    adapter

--- rest/rest.go
+++ rest/rest.go
@@ -24,11 +24,11 @@ func (u url) MarshalText() ([]byte, error) {
 }
 
 type urlDescription struct {
-	// 구조체의 앞글자가 소문자로 시작하면 해당 필드는 export하지 않기 때문에 무조건 대문자로 시작해야한다. 
+	// 구조체의 앞글자가 소문자로 시작하면 해당 필드는 export하지 않기 때문에 무조건 대문자로 시작해야한다.
 	// 만약 json 필드를 전부 소문자로(혹은 다른 글자로) 하고 싶으면 아래와 같이 백틱을 사용해서 json에서 어떻게 보여질 지 명시할 수 있다.
 	// Field Tags를 검색하면 백틱에 들어가는 것들에 대한 정보를 더 얻을 수 있다.
-	URL url `json:"url"`
-	Method string `json:"method"`
+	URL         url    `json:"url"`
+	Method      string `json:"method"`
 	Description string `json:"description"`
 
 	Payload string `json:"payload,omitempty"` // omitempty가 들어가면 만약 값이 비어있다면 아예 json에 값을 넣지 않는다.
@@ -49,31 +49,31 @@ func (u urlDescription) String() string {
 }
 
 func documentation(rw http.ResponseWriter, r *http.Request) {
-	data := []urlDescription {
+	data := []urlDescription{
 		{
-			URL: url("/"),
-			Method: "GET",
+			URL:         url("/"),
+			Method:      "GET",
 			Description: "See Documentation",
 		},
 		{
-			URL: url("/blocks"),
-			Method: "GET",
+			URL:         url("/blocks"),
+			Method:      "GET",
 			Description: "See All Blocks",
 		},
 		{
-			URL: url("/blocks"),
-			Method: "POST",
+			URL:         url("/blocks"),
+			Method:      "POST",
 			Description: "Add A Block",
-			Payload: "data:string",
+			Payload:     "data:string",
 		},
 		{
-			URL: url("/blocks/{height}"),
-			Method: "GET",
+			URL:         url("/blocks/{height}"),
+			Method:      "GET",
 			Description: "See A Block",
 		},
 	}
-	rw.Header().Add("Content-Type", "application/json")
-	
+	//rw.Header().Add("Content-Type", "application/json")
+
 	// b, err := json.Marshal(data) // 구조체를 json으로 바꿔줌(byte array로 바뀜)
 	// utils.HandleErr(err)
 	// fmt.Fprintf(rw, "%s\n", b)
@@ -83,9 +83,9 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 func blocks(rw http.ResponseWriter, r *http.Request) {
 	rw.Header().Add("Content-Type", "application/json")
 	switch r.Method {
-	case "GET" :
+	case "GET":
 		json.NewEncoder(rw).Encode(blockchain.GetInstance().AllBlocks())
-	case "POST" : 
+	case "POST":
 		var addBlockBody addBlockBody
 		utils.HandleErr(json.NewDecoder(r.Body).Decode(&addBlockBody))
 		blockchain.GetInstance().AddBlock(addBlockBody.Message)
@@ -103,7 +103,16 @@ func block(rw http.ResponseWriter, r *http.Request) {
 		encoder.Encode(errorResponse{fmt.Sprint(err)})
 	} else {
 		encoder.Encode(block)
-	}	
+	}
+}
+
+// 미들웨어
+func jsonContentTypeMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Add("Content-Type", "application/json")
+		// 필수
+		next.ServeHTTP(rw, r)
+	})
 }
 
 func Start(aPort int) {
@@ -111,15 +120,18 @@ func Start(aPort int) {
 	// Mux는 HandleFunc 함수에서 url과 handler를 매핑시켜주는 역할을 한다.
 	// 만약 사용할 Mux를 ListenAndServe 함수에 파라미터로 넣지 않는다면 DefaultServeMux를 사용한다.
 	// handler := http.NewServeMux()
-	
+
 	router := mux.NewRouter()
 	port = fmt.Sprintf(":%d", aPort)
-	
+
 	// http.HandleFunc("/", documentation)
 	// http.HandleFunc("/blocks", blocks)
 	// handler.HandleFunc("/", documentation)
 	// handler.HandleFunc("/blocks", blocks)
-	
+
+	// 모든 router.HandleFunc()이 호출되기 전에 실행된다.
+	router.Use(jsonContentTypeMiddleware)
+
 	// gorilla mux는 핸들러가 어떤 http method를 다루는지 지정할 수 있다.
 	router.HandleFunc("/", documentation).Methods("GET")
 	router.HandleFunc("/blocks", blocks).Methods("GET", "POST")
